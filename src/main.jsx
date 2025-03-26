1a43710c47b3a062045347f991577bbc6930fb4e
moqam <qamar@moqam.ca>
2025-03-26 13:20:11 -0400

    mc

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import clarity from 'clarity-js'; // Import Clarity
 
-import App from "./App";
-import "./index.css";
+// Initialize Clarity with your project ID
+clarity.start({ projectId: 'quct0jgx9f' });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
