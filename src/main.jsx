0c9dc4b0fb787fd3a2268b2bc4541ab05174d755
moqam <qamar@moqam.ca>
2025-03-26 13:22:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import clarity from 'clarity-js'; // Import Clarity
+import Clarity from '@microsoft/clarity';
+
+const projectId = 'quct0jgx9f';
 
 // Initialize Clarity with your project ID
-clarity.start({ projectId: 'quct0jgx9f' });
+Clarity.init(projectId);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
